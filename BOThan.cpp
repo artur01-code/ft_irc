4e647a49acabe6371ad4361e04060f47628e0a29
Jonas Subel <joooni.su@gmail.com>
2022-11-30 16:38:51 +0100

    Bot class done, now need to add its functionality to privmsg, channel, yada yada

---
+++ BOThan.cpp
@@ -0,0 +1,53 @@
+#include "BOThan.hpp"
+
+BOThan::BOThan()
+{
+
+}
+
+BOThan::BOThan(Client *cl): _botClient(cl), _dictionary(std::map<std::string, std::string>)
+{
+	this->_botClient->setNickname("BOThan");
+	this->_botClient->setRealname("BOTh4n Will-1-4MS");
+	this->_botClient->setUsername("Mimo de Pozor");
+	this->_botClient->setHostname("Wales");
+	this->_dictionary["nice"] = "neis iawn";
+	this->_dictionary["Say something welsh"] = "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch";
+	this->_dictionary["fuck"] = "chnachu bant, bruv";
+}
+
+BOThan::~BOThan()
+{
+
+}
+
+Client *BOThan::getBotClient()
+{
+	return (this->_botClient);
+}
+
+bool	BOThan::addToDict(std::string word, std::string phrase)
+{
+	if (!(this->_dictionary.insert(std::pair<std::string, std::string>(word, phrase)).second()))
+		return (false);
+	return (true);
+}
+
+bool	BOThan::removeFromDict(std::string word)
+{
+	if (!(this->_dictionary.erase(word)))
+		return (false);
+	return (true);
+}
+
+std::string	BOThan::getPhraseFromDict(std::string word)
+{
+	return (_dictionary[word]);
+}
+
+bool BOThan::checkBethavious(std::string text)
+{
+	if (!(this->_dictionary.count(text)))
+		return (false);
+	return (true);
+}
---
+++ BOThan.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef BOTHAN_HPP
+#define BOTHAN_HPP
+
+#ifndef M_DEBUG
+#define M_DEBUG	0
+#endif
+
+#include <string>
+#include <utility>
+#include <iostream>
+
+#include <map>
+
+#include	"Colors.hpp"
+#include	"Client.hpp"
+
+Class Client;
+
+class BOThan
+{
+	public:
+		BOThan();
+		BOThan(Client *cl);
+		~BOThan();
+
+		Client		*getBotClient();
+		bool		addToDict(std::string word, std::string phrase);
+		bool		removeFromDict(std::string word);
+		std::string	getPhraseFromDict(std::string word);
+		bool		checkBethaviour(std::string text);
+	private:
+		BOThan(const BOThan &rhs);
+		BOThan &operator=(const BOThan &rhs);
+		Client *_botClient;
+		std::map<std::string, std::string> _dictionary;
+};
+
+#endif
